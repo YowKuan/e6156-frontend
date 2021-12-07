6049f1cff4d29e7ee2b094b8f822bf456b65bf5c
chiajoukuo <b05901166@ntu.edu.tw>
2021-12-07 16:54:44 -0500

    set up bootstrap

---
+++ front-end-6156/src/components/Mycomponent/Mycomponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Mycomponent.module.css';
+
+const Mycomponent = () => (
+  <div className={styles.Mycomponent}>
+    Mycomponent Component
+  </div>
+);
+
+Mycomponent.propTypes = {};
+
+Mycomponent.defaultProps = {};
+
+export default Mycomponent;
---
+++ front-end-6156/src/pages/CreateDraft.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from '../components/NavBar/NavBar';
+
+const CreateDraft = () => (
+  <div>
+    <NavBar></NavBar>
+  </div>
+);
+
+CreateDraft.propTypes = {};
+
+CreateDraft.defaultProps = {};
+
+export default CreateDraft;
