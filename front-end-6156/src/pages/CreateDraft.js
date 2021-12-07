a752b6be622b762040a194be349ac6b32f7c5a7d
chiajoukuo <b05901166@ntu.edu.tw>
2021-12-07 17:29:26 -0500

    add tabs

--- front-end-6156/src/pages/CreateDraft.js
+++ front-end-6156/src/pages/CreateDraft.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Tab, Tabs} from 'react-bootstrap';
 import NavBar from '../components/NavBar/NavBar';
 
 const CreateDraft = () => (
   <div>
-    Create Draft
+    <h1>Create Draft</h1>
+    <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+      <Tab eventKey="rules" title="Rules">
+        rules
+      </Tab>
+      <Tab eventKey="roster" title="Roster">
+        roster
+      </Tab>
+      <Tab eventKey="scoring" title="Scoring">
+        scoring
+      </Tab>
+      <Tab eventKey="teams" title="Teams">
+        scoring
+      </Tab>    
+    </Tabs>
   </div>
 );
 
