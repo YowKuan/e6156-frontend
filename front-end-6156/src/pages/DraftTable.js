e9f02d1c1345b70dacc2c763117f429b57552e4e
chiajoukuo <b05901166@ntu.edu.tw>
2021-12-16 17:56:19 -0500

    draft basic finish

--- front-end-6156/src/pages/DraftTable.js
+++ front-end-6156/src/pages/DraftTable.js
@@ -4,15 +4,15 @@ import {Tab, Tabs, Card, Button, Container, Col, Row, Image} from 'react-bootstr
 import { DataGrid } from '@mui/x-data-grid';
 import axios from 'axios';
 import {PLAYERS} from '../helper/constants'
+import {Link} from "react-router-dom";
 
 class DraftTable extends Component {
   constructor(props){
     super(props);
     this.state = {
-      user_id: 'test@columbia.edu',
       gamers: [], // num => string
       gamers_map: {}, // tring => num
-      current_gamer_id: -1,
+      current_gamer_id: 0,
       current_tab_id: 0,
       players: [], // num => data(string)
       players_map: {}, // string => num
@@ -48,6 +48,8 @@ class DraftTable extends Component {
     };
     this.choosePlayer = this.choosePlayer.bind(this)
     this.setKey = this.setKey.bind(this)
+    this.load = this.load.bind(this)
+    this.endGame = this.endGame.bind(this)
   }
 
   // 1. photo
@@ -55,30 +57,7 @@ class DraftTable extends Component {
   //
   // onclick => rerender
   // player need an extra field chosen
-
-  UNSAFE_componentWillMount() {
-
-    // for(let i=0; i<20; i++){
-    //   players.push({id:i, name:'player '.concat(String(i))})
-    // }
-
-    // axios.get('http://54.208.206.103:5000/api/lookup/E6156player')
-    // .then(res => {
-    //   players = res['data'];
-    //   for (let i=0; i<players.length; i++){
-    //     players_map[players[i].id] = i
-    //     players[i].chosen = false
-    //   }
-    //   for (let i=0; i<gamers.length; i++){
-    //     teams_member.push([players[0], players[1]])
-    //   }
-    //   this.setState({
-    //     gamers: gamers,
-    //     players: players,
-    //     current_gamer_id: 0,
-    //     players_map: players_map,
-    //     teams_member: teams_member
-    //   })
+  load(){
     let gamers = [] //[{id: 0, name:'gamer 0'}, {id: 1, name:'gamer 1'}, {id: 2, name:'gamer 2'}]
     let gamers_map = {}
     let players = [] //[{id: 1, name:'palyer1'}, {id: 2, name:'palyer2'}, {id: 3, name:'palyer3'}]
@@ -89,6 +68,7 @@ class DraftTable extends Component {
     const draft_url = 'http://35.171.161.15:8111/draft/'.concat(this.props.match.params.id)
     const players_request = axios.get(players_url)
     const draft_request = axios.get(draft_url)
+
     axios.all([players_request, draft_request]).then(axios.spread((...responses) => {
       players = responses[0]['data'];
       let teams_member_dict = responses[1].data.teams
@@ -111,33 +91,76 @@ class DraftTable extends Component {
         gamers: gamers,
         gamers_map: gamers_map,
         players: players,
-        current_gamer_id: 0,
         players_map: players_map,
         teams_member: teams_member,
       });
+    }))
+  };
 
-    }));
+  UNSAFE_componentWillMount() {
+    this.load();
+    // let gamers = [] //[{id: 0, name:'gamer 0'}, {id: 1, name:'gamer 1'}, {id: 2, name:'gamer 2'}]
+    // let gamers_map = {}
+    // let players = [] //[{id: 1, name:'palyer1'}, {id: 2, name:'palyer2'}, {id: 3, name:'palyer3'}]
+    // let players_map = {}
+    // let teams_member = []
 
-
+    // const players_url = 'http://54.208.206.103:5000/api/lookup/E6156player'
+    // const draft_url = 'http://35.171.161.15:8111/draft/'.concat(this.props.match.params.id)
+    // const players_request = axios.get(players_url)
+    // const draft_request = axios.get(draft_url)
+    // axios.all([players_request, draft_request]).then(axios.spread((...responses) => {
+    //   players = responses[0]['data'];
+    //   let teams_member_dict = responses[1].data.teams
+    //   for (let i=0; i<players.length; i++){
+    //     players_map[players[i].id.toString()] = i
+    //     players[i].chosen = false
+    //   }
+    //   let gamer_idx = 0
+    //   for (const [gamer, team] of Object.entries(teams_member_dict)){
+    //     gamers.push(gamer)
+    //     gamers_map[gamer] = gamer_idx
+    //     teams_member.push([])
+    //     for (let i=0; i<team.length; i++){
+    //       teams_member[gamer_idx].push(players[players_map[team[i]]])
+    //       players[players_map[team[i]]].chosen = true
+    //     }
+    //     gamer_idx += 1
+    //   }
+    //   this.setState({
+    //     gamers: gamers,
+    //     gamers_map: gamers_map,
+    //     players: players,
+    //     current_gamer_id: 0,
+    //     players_map: players_map,
+    //     teams_member: teams_member,
+    //   });
+    // }));
   }
 
   choosePlayer(e){
-    console.log('choose player', e.target.id, typeof(e.target.id))
-    console.log(e)
-    this.state.players[this.state.players_map[e.target.id]].chosen = true
+    // console.log('choose player', e.target.id, typeof(e.target.id))
+    // console.log(e)
+    // this.state.players[this.state.players_map[e.target.id]].chosen = true
     let player_id = e.target.id
-    // this.state.teams_member[this.state.current_gamer_id].push(player_id)
-    console.log(this.state.teams_member[this.state.current_gamer_id])
-    const new_player = this.state.players[this.state.players_map[player_id]]
-    console.log(new_player)
-    // this.state.teams_member[this.state.current_gamer_id].push(new_player)
-    this.state.teams_member[this.state.current_gamer_id] = [...this.state.teams_member[this.state.current_gamer_id], new_player]
-    let next_gamer = (this.state.current_gamer_id+1)%this.state.gamers.length
-    this.setState({
-      current_gamer_id: next_gamer,
-      current_tab_id: next_gamer,
+    // const new_player = this.state.players[this.state.players_map[player_id]]
+    // this.state.teams_member[this.state.current_gamer_id] = [...this.state.teams_member[this.state.current_gamer_id], new_player]
+    const request = 'http://35.171.161.15:8111/draft/select/'.concat(this.props.match.params.id, '/', this.state.gamers[this.state.current_gamer_id], '/', player_id)
+    axios.put(request).then(res => {
+      this.load()
+      let next_gamer = (this.state.current_gamer_id+1)%this.state.gamers.length
+      this.setState({
+        current_gamer_id: next_gamer,
+        current_tab_id: next_gamer,
+      })
     })
-    console.log(this.state.teams_member)
+
+
+  }
+  endGame(){
+    console.log('end game')
+    console.log(this.props.history)
+    // this.props.history.push('/draft')
   }
 
   setKey(k){
@@ -147,18 +170,9 @@ class DraftTable extends Component {
   }
 
   render() {
-    // console.log('render', this.state.teams_member)
-    //                           // this.state.teams_member[gamer.id].map(palyerId => (
-    //                       //   this.state.players[this.state.players_map[palyerId]]
-                            
-    //                       // ))
-    // const teams = []
-    // for(let gamerId=0; gamerId<this.state.gamers.length; gamerId++){
-    //   for (let playerId=0; playerId<)
-    //   teams.push(this.state.players[this.state.players_map[i]])
-    // }
     const id = this.props.match.params.id
     console.log(this.state)
+    console.log(this.state.current_gamer_id)
     return (
       <div>
         <h1>Draft</h1>
@@ -166,9 +180,9 @@ class DraftTable extends Component {
         <Container fluid='md'>
           <Row>
             <Card style={{paddingBottom: '10px'}}>
-              <Tabs defaultActiveKey="0" id="uncontrolled-tab-example" className="mb-3 mt-2">
-                {this.state.gamers.map(gamer => (
-                  <Tab eventKey={gamer} title={gamer} key={gamer}>
+              <Tabs defaultActiveKey={this.state.current_tab_id} onSelect={(k) => this.setKey(k)} id="uncontrolled-tab-example" className="mb-3 mt-2">
+                {this.state.gamers.map((gamer, idx) => (
+                  <Tab eventKey={idx} title={gamer} key={gamer}>
                     <div style={{ height: 300, width: '100%' }}>
                       <DataGrid
                         rows={this.state.teams_member[this.state.gamers_map[gamer]]}
@@ -199,13 +213,20 @@ class DraftTable extends Component {
                 <DataGrid
                   rows={this.state.players}
                   columns={this.state.columns}
+                  pagination
                   pageSize={10}
-                  // rowsPerPageOptions={[5]}
                   // checkboxSelection
+                  // rowsPerPageOptions={[10, 20, 50]}
+                  // onPageChange={(newPage) => setPage(newPage)}
+                  
                 />
               </div>
             </Card>
           </Row>
+          <Link to='/draft'>
+            <Button onClick={this.endGame()} style={{marginTop: '5px', marginBottom: '10px'}} size="lg">See Result</Button>
+          </Link>
+
         </Container>
       </div>
     );
