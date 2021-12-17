ea653e0b25ab725f0eb6f67473e0ed34839f6743
YowKuan <yoyo6213@gmail.com>
2021-12-17 11:52:48 -0500

    NavBar change to link

--- front-end-6156/src/App.js
+++ front-end-6156/src/App.js
@@ -17,23 +17,23 @@ function App() {
   Cookies.set('cookie_name', 'value')
   console.log(Cookies.get('cookie_name'))
 
-  function loginFunc(){
-    axios.get(baseURL).then((response)=>{
-      console.log(response)
-      if (response.status === 200){
-        console.log(response)
+  // function loginFunc(){
+  //   axios.get(baseURL).then((response)=>{
+  //     console.log(response)
+  //     if (response.status === 200){
+  //       console.log(response)
 
-      }   
-    }).catch(error => {
-      console.log(error)
-      return error
-    });
+  //     }   
+  //   }).catch(error => {
+  //     console.log(error)
+  //     return error
+  //   });
 
-  }
+  // }
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar loginFunc={loginFunc}></NavBar>
+        <NavBar></NavBar>
         <div className="App">
           <Switch>
             <Route exact path='/create_draft' render={(props) => <CreateDraft/>} />
